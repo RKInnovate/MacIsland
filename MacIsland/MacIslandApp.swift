6279b7803780b2042ab05985067409e92d8c489a
badrat-in <rbmorena42@gmail.com>
2024-08-10 17:13:22 +0530

    Initial Commit

---
+++ MacIsland/MacIslandApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacIslandApp.swift
+//  MacIsland
+//
+//  Created by Ravindra Singh on 10/08/24.
+//
+
+import SwiftUI
+
+@main
+struct MacIslandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MacIslandUITests/MacIslandUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MacIslandUITestsLaunchTests.swift
+//  MacIslandUITests
+//
+//  Created by Ravindra Singh on 10/08/24.
+//
+
+import XCTest
+
+final class MacIslandUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
