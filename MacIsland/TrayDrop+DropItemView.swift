780abbe7d071f57eb3dce60672b7deee2c70b97a
badrat-in <rbmorena42@gmail.com>
2024-08-10 18:56:47 +0530

    Create TrayDrop

---
+++ MacIsland/TrayDrop+DropItemView.swift
@@ -0,0 +1,63 @@
+//
+//  TrayDrop+DropItemView.swift
+//  MacIsland
+//
+//  Created by Ravindra Singh on 10/08/24.
+//
+
+import Foundation
+import Pow
+import SwiftUI
+import UniformTypeIdentifiers
+
+struct DropItemView: View {
+    let item: TrayDrop.DropItem
+    @StateObject var vm: DynamicIslandViewModel
+    @StateObject var tvm = TrayDrop.shared
+
+    @State var hover = false
+
+    var body: some View {
+        VStack {
+            Image(nsImage: item.workspacePreviewImage)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(maxWidth: 64)
+            Text(item.fileName)
+                .multilineTextAlignment(.center)
+                .font(.system(.footnote, design: .rounded))
+                .frame(maxWidth: 64)
+        }
+        .contentShape(Rectangle())
+        .transition(.asymmetric(
+            insertion: .opacity.combined(with: .scale),
+            removal: .movingParts.poof
+        ))
+        .contentShape(Rectangle())
+        .onHover { hover = $0 }
+        .scaleEffect(hover ? 1.05 : 1.0)
+        .animation(vm.animation, value: hover)
+        .onDrag { NSItemProvider(contentsOf: item.storageURL) ?? .init() }
+        .onTapGesture {
+            guard !vm.optionKeyPressed else { return }
+            vm.notchClose()
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                NSWorkspace.shared.open(item.storageURL)
+            }
+        }
+        .overlay {
+            Image(systemName: "xmark.circle.fill")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .foregroundStyle(.red)
+                .background(Color.white.clipShape(Circle()).padding(1))
+                .frame(width: vm.spacing, height: vm.spacing)
+                .opacity(vm.optionKeyPressed ? 1 : 0)
+                .scaleEffect(vm.optionKeyPressed ? 1 : 0.5)
+                .animation(vm.animation, value: vm.optionKeyPressed)
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topTrailing)
+                .offset(x: vm.spacing / 2, y: -vm.spacing / 2)
+                .onTapGesture { tvm.delete(item.id) }
+        }
+    }
+}
---
+++ MacIsland/TrayDrop+View.swift
@@ -0,0 +1,93 @@
+import SwiftUI
+
+struct TrayView: View {
+    @StateObject var vm: DynamicIslandViewModel
+    @StateObject var tvm = TrayDrop.shared
+
+    @State private var targeting = false
+
+    var storageTime: String {
+        switch tvm.selectedFileStorageTime {
+        case .oneHour:
+            return NSLocalizedString("an hour", comment: "")
+        case .oneDay:
+            return NSLocalizedString("a day", comment: "")
+        case .twoDays:
+            return NSLocalizedString("two days", comment: "")
+        case .threeDays:
+            return NSLocalizedString("three days", comment: "")
+        case .oneWeek:
+            return NSLocalizedString("a week", comment: "")
+        case .never:
+            return NSLocalizedString("forever", comment: "")
+        case .custom:
+            let localizedTimeUnit = NSLocalizedString(tvm.customStorageTimeUnit.localized.lowercased(), comment: "")
+            return "\(tvm.customStorageTime) \(localizedTimeUnit)"
+        }
+    }
+
+    var body: some View {
+        panel
+            .onDrop(of: [.data], isTargeted: $targeting) { providers in
+                DispatchQueue.global().async { tvm.load(providers) }
+                return true
+            }
+    }
+
+    var panel: some View {
+        RoundedRectangle(cornerRadius: vm.cornerRadius)
+            .strokeBorder(style: StrokeStyle(lineWidth: 4, dash: [10]))
+            .foregroundStyle(.white.opacity(0.1))
+            .background(loading)
+            .overlay {
+                content
+                    .padding()
+            }
+            .animation(vm.animation, value: tvm.items)
+            .animation(vm.animation, value: tvm.isLoading)
+    }
+
+    var loading: some View {
+        RoundedRectangle(cornerRadius: vm.cornerRadius)
+            .foregroundStyle(.white.opacity(0.1))
+            .conditionalEffect(
+                .repeat(
+                    .glow(color: .blue, radius: 50),
+                    every: 1.5
+                ),
+                condition: tvm.isLoading > 0
+            )
+    }
+
+    var content: some View {
+        Group {
+            if tvm.isEmpty {
+                VStack(spacing: 8) {
+                    Image(systemName: "tray.and.arrow.down.fill")
+
+                    Text(NSLocalizedString("Drag files here to keep them for", comment: "") + " " + storageTime + " " + NSLocalizedString("& Press Option to delete", comment: ""))
+                        .font(.system(.headline, design: .rounded))
+                }
+            } else {
+                ScrollView(.horizontal) {
+                    HStack(spacing: vm.spacing) {
+                        ForEach(tvm.items) { item in
+                            DropItemView(item: item, vm: vm, tvm: tvm)
+                        }
+                    }
+                    .padding(vm.spacing)
+                }
+                .padding(-vm.spacing)
+                .scrollIndicators(.never)
+            }
+        }
+    }
+}
+
+#Preview {
+    DynamicIslandContentView(vm: .init())
+        .padding()
+        .frame(width: 550, height: 150, alignment: .center)
+        .background(.black)
+        .preferredColorScheme(.dark)
+}
